832b3f4265198979204ba9a80d86fe26f0589090
Edu Walter Armas Aliaga <edu_armas11@hotmail.com>
2023-08-30 12:23:45 -0500

    delete dist

--- dist/dao/models/githubUserSchema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const githubUserSchema = new mongoose.Schema({
-  accountId: String,
-  name: String,
-  provider: String
-});
-const GithubUser = mongoose.model("GithubUser", githubUserSchema);
-export default GithubUser;
